9cf638233ab2f5390ef6dbbf3890366ab51371a1
baba_s <shotababa1206@gmail.com>
2020-08-20 17:07:54 +0900

    first commit

---
+++ Editor/GameObjectLocker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	internal static class GameBojectLocker
+	{
+		[MenuItem( "CONTEXT/Transform/Switch Lock - Unlock" )]
+		private static void Lock( MenuCommand command )
+		{
+			var transform  = ( Transform ) command.context;
+			var gameObject = transform.gameObject;
+
+			gameObject.hideFlags ^= HideFlags.NotEditable;
+
+			EditorUtility.SetDirty( gameObject );
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# UniGameObjectLocker
